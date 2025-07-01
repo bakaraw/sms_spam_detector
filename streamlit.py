02f2326d8a25b1aa919eb6632ddefa388e6a35f5
bakaraw <bacalso.ton2x@gmail.com>
2025-07-01 20:49:38 +0800

    updated streamlit.py
    - loading the pre trained model instead of training the model again

--- streamlit.py
+++ streamlit.py
@@ -14,45 +14,50 @@ st.title("📩 SMS Spam Detector with Naive Bayes")
 
 @st.cache_data
 def load_data():
-    path = kagglehub.dataset_download("uciml/sms-spam-collection-dataset")
-    df = pd.read_csv(f"{path}/spam.csv", encoding='latin-1')[['v1', 'v2']]
-    df.columns = ['label', 'text']
-    df['text'] = df['text'].str.lower()
-    return df
+    try:
+        model = joblib.load('model/model.joblib')
+        vectorizer = joblib.load('model/vectorizer.joblib')
+    except Exception as e:
+        print(f"Error in loading model :( {e}")
+        return None, None
+
+    return model, vectorizer
 
 # Train model and save
 
 
-@st.cache_resource
-def train_and_save_model(df):
-    X_train, X_test, y_train, y_test = train_test_split(
-        df['text'], df['label'], test_size=0.2, random_state=42
-    )
-
-    vectorizer = CountVectorizer()
-    X_train_vec = vectorizer.fit_transform(X_train)
-
-    model = MultinomialNB()
-    model.fit(X_train_vec, y_train)
-
-    os.makedirs("model", exist_ok=True)
-    joblib.dump(model, "model/model.joblib")
-    joblib.dump(vectorizer, "model/vectorizer.joblib")
-
-    return model, vectorizer
+# @st.cache_resource
+# def train_and_save_model(df):
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         df['text'], df['label'], test_size=0.2, random_state=42
+#     )
+#
+#     vectorizer = CountVectorizer()
+#     X_train_vec = vectorizer.fit_transform(X_train)
+#
+#     model = MultinomialNB()
+#     model.fit(X_train_vec, y_train)
+#
+#     os.makedirs("model", exist_ok=True)
+#     joblib.dump(model, "model/model.joblib")
+#     joblib.dump(vectorizer, "model/vectorizer.joblib")
+#
+#     return model, vectorizer
 
 
 # Load data and train model
-with st.spinner("Loading and training model..."):
-    df = load_data()
-    model, vectorizer = train_and_save_model(df)
+with st.spinner("Loading model..."):
+    model, vectorizer = load_data()
+
+if not model and not vectorizer:
+    st.error("Model loading failed")
 
 st.success("Model is ready!")
 
 # Text input
 user_input = st.text_input("📨 Enter an SMS message to classify:")
 
-if user_input:
+if user_input and model and vectorizer:
     X_input = vectorizer.transform([user_input.lower()])
     prediction = model.predict(X_input)[0]
     st.write(f"🧠 Prediction: **{prediction.upper()}**")
